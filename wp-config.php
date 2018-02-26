94d75ec8c52e3392a0f0414d018c598ffc2b0630
Chris Gold <chris.ratsimba87@gmail.com>
2018-02-26 12:08:14 +0100

    first commit

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'purple');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Alchimie12');
+define('DB_PASSWORD', 'fuckoff');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
